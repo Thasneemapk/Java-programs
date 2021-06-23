c779dd496d48072dd660174772c33b76f7dacd4a
Thasneemapk <80021459+Thasneemapk@users.noreply.github.com>
2021-06-23 20:46:14 +0530

    Add files via upload

---
+++ user input/arraybuffer.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class arraybuffer {
+    public static void main(String[] args) throws IOException {
+        int i, length;
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter the number of elements:");
+        length = Integer.parseInt(read.readLine());
+        int[] array = new int[length];
+        System.out.println("Enter the elements of array:");
+        for(i=0; i < length; i++){
+            array[i] =Integer.parseInt(read.readLine());
+        }
+        System.out.println("\nThe array elements are:");
+        for (i=0; i<length; i++)
+        {
+            System.out.println(array[i]);
+        }
+
+    }
+}
